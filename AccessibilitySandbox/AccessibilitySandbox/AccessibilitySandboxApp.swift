fa6c3e3a6cbd22f435db1c57bc6afbc2ce8d3302
mapledeveh <mapledeveh@gmail.com>
2023-08-06 18:41:53 -0400

    Update Progress

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Alex Nguyen on 2023-07-30.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
